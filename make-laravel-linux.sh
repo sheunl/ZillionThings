3256aad9622ffbd04cf1a2f39cf90d012f75c260
Abdulrasaq Lawani <abdulrasaqolawani@gmail.com>
2023-10-01 18:25:00 -0400

    Added the example folder

--- make-laravel-linux.sh
+++ make-laravel-linux.sh
@@ -5,3 +5,4 @@ sudo chmod 777 -R www/storage
 sed -ibak 's/mailpit/mailhog/' www/.env 
 echo '*' >> www/.gitignore
 echo '!.gitignore' >> www/.gitignore
+echo 'This is a test page' > www/resources/views/welcome.blade.php
