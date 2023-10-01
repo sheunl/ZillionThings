f792c7cc38993c6f771cf5c9059ea7b0c66c568c
Abdulrasaq Lawani <abdulrasaqolawani@gmail.com>
2023-10-01 17:47:52 -0400

    Added the example folder

--- make-laravel-linux.sh
+++ make-laravel-linux.sh
@@ -2,3 +2,6 @@
 
 composer create-project laravel/laravel www
 sudo chmod 777 -R www/storage
+sed -ibak 's/mailpit/mailhog/' www/.env 
+echo '*' >> www/.gitignore
+echo '!.gitignore' >> www/.gitignore
